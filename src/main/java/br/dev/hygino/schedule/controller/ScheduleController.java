7fc1563d0d14fbf7767fb531019e07016ece882b
Adroaldo <hygino@msn.com>
2026-02-10 20:51:56 -0300

    feat: remove schedule

--- src/main/java/br/dev/hygino/schedule/controller/ScheduleController.java
+++ src/main/java/br/dev/hygino/schedule/controller/ScheduleController.java
@@ -1,18 +1,21 @@
 package br.dev.hygino.schedule.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.dev.hygino.schedule.dto.RequestScheduleDto;
@@ -61,5 +64,14 @@ public class ScheduleController {
         var result = scheduleService.modifySchedule(dto);
 
         return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> removeSchedule(
+            @RequestParam String client,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime dateTime) {
+        scheduleService.removeSchedule(client, dateTime);
+
+        return ResponseEntity.noContent().build();
     }
 }
